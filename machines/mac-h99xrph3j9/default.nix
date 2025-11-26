f5aec41415faf792d5a70db61ceb8ac1af52d49f
Chris Becker <christopher.becker@ro.co>
2025-11-26 10:22:28 -0500

    add work machine

---
+++ machines/mac-h99xrph3j9/default.nix
@@ -0,0 +1,56 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+let
+  username = "christopher.becker";
+  userHome = "/Users/christopher.becker";
+  localConfig = ./local.nix;
+in
+{
+  imports = [
+    ../../bootstrap/darwin.nix
+    ../../modules/darwin/defaults.nix
+    ../../modules/emacs-macport.nix
+  ]
+  ++ lib.optionals (builtins.pathExists localConfig) [ localConfig ];
+
+  system.primaryUser = username;
+
+  _module.args = { inherit username userHome; };
+
+  nix-homebrew = {
+    enable = true;
+    enableRosetta = true;
+    user = username;
+    autoMigrate = true;
+    mutableTaps = true;
+  };
+
+  custom.homebrew.excludeCasks = [ "contexts" ];
+
+  home-manager.users.${username} = {
+    programs.git.settings.user.email = "REDACTED";
+    home.packages = with pkgs; [
+      pkgs.pkgs-master.claude-code
+      aws-sso-cli
+
+      nodePackages_latest.prettier
+    ];
+  };
+
+  system.defaults.universalaccess = { };
+
+  homebrew.taps = [ ];
+
+  homebrew.brews = [ ];
+
+  homebrew.casks = [
+    "yaak"
+  ];
+
+  # TODO: handle mac app store apps?
+  # utc-time - https://sindresorhus.com/utc-time
+  # dato - https://sindresorhus.com/dato
+}
