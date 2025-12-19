5cd8452bc71cdca76e305800940b349a12e2bc20
Chris Becker <christopher.becker@ro.co>
2025-12-19 10:47:23 -0500

    more work machine setup

--- machines/mac-h99xrph3j9/default.nix
+++ machines/mac-h99xrph3j9/default.nix
@@ -19,8 +19,30 @@ in
   ];
 
   system.primaryUser = username;
+  system.defaults.universalaccess = { }; # work profile blocks changing this
+
+  services.caffeinate.enable = true;
 
   _module.args = { inherit username userHome; };
+
+  home-manager.users.${username} = {
+    identity.email = workEmail;
+
+    home.packages = with pkgs; [
+      pkgs.pkgs-master.claude-code
+      pkgs.pkgs-master.codex
+      aws-sso-cli
+      terraform_1_5_7
+      terragrunt
+
+      nodePackages_latest.prettier
+    ];
+  };
+
+  custom.emacs = {
+    macMetal = true;
+    cflags.value = "-O3 -mcpu=native -fobjc-arc -DFD_SETSIZE=10000 -D_DARWIN_UNLIMITED_SELECT";
+  };
 
   nix-homebrew = {
     enable = true;
@@ -56,6 +78,8 @@ in
   ];
 
   # TODO: handle mac app store apps?
-  # utc-time - https://sindresorhus.com/utc-time
-  # dato - https://sindresorhus.com/dato
+  # 1PW for Safari - 1569813296
+  # utc time - 1538245904
+  # dato - 1470584107
+  # wipr - 1662217862
 }
