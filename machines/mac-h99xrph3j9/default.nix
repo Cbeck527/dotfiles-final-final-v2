634d001c904383b5b79f33c25768eb001b1f3890
Chris Becker <christopher.becker@ro.co>
2026-03-06 17:54:29 -0500

    work: add datadog's pup cli

--- machines/mac-h99xrph3j9/default.nix
+++ machines/mac-h99xrph3j9/default.nix
@@ -6,6 +6,22 @@ let
   username = "christopher.becker";
   userHome = "/Users/christopher.becker";
   workEmail = "REDACTED";
+
+  datadog-pup = pkgs.stdenv.mkDerivation (finalAttrs: {
+    pname = "datadog-pup";
+    version = "0.27.0";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/DataDog/pup/releases/download/v${finalAttrs.version}/pup_${finalAttrs.version}_Darwin_arm64.tar.gz";
+      hash = "sha256-beHU+qv/wv9Fa3oBHlGKjipoIF2Wcnq8CFz1xlyeVwk=";
+    };
+
+    sourceRoot = ".";
+
+    installPhase = ''
+      install -Dm755 pup $out/bin/pup
+    '';
+  });
 in
 {
   imports = [
